70b5912f6bba1aef2ee759ddeea05904aca910ec
aldo <aldomatus@getzuno.com>
2023-10-30 13:27:13 -0600

    ADD: interfaces file to understand interfaces implementation in golang

---
+++ interfaces/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	name string
+	age  int
+}
+
+type Employee struct {
+	id int
+}
+
+type FullTimeEmployee struct {
+	Person
+	Employee
+	endDate string
+}
+
+func (ftEmployee FullTimeEmployee) getMessage() string {
+	return "Full Time Employee"
+}
+
+type TemporaryEmployee struct {
+	Person
+	Employee
+	taxRate int
+}
+
+func (tEmployee TemporaryEmployee) getMessage() string {
+	return "Temporary Employee"
+}
+
+type PrintInfo interface {
+	getMessage() string
+}
+
+func getMessage(p PrintInfo) {
+	fmt.Println(p.getMessage())
+}
+
+func main() {
+	ftEmployee := FullTimeEmployee{}
+	tEmployee := TemporaryEmployee{}
+
+	getMessage(ftEmployee)
+	getMessage(tEmployee)
+}
